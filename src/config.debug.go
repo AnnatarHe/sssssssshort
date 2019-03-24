733e0f21fccaefae48cb5531616105bb7bcc7c2d
le.he <Annatar.He@liulishuo.com>
2019-03-24 10:24:59 +0800

    feat(sqlite): resolve #2

--- src/config.debug.go
+++ src/config.debug.go
@@ -3,7 +3,7 @@
 package src
 
 const (
-	DB_SRC     = "short:password@(localhost)/short"
+	DB_SRC     = "file:./db.db?cache=shared&mode=rw"
 	API_LISTEN = "localhost:9999"
 	HOST_URL   = "http://localhost:9999/"
 )
--- src/init.go
+++ src/init.go
@@ -2,12 +2,14 @@ package src
 
 import (
 	"github.com/jmoiron/sqlx"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sqlx.DB
 
 func InitDB() {
-	dbConnect, err := sqlx.Open("mysql", DB_SRC)
+	dbConnect, err := sqlx.Open("sqlite3", DB_SRC)
 	if err != nil {
 		panic(err)
 	}
