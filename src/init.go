7400404d1bf472a5edb21be604d5805dc4858f0a
AnnatarHe <iamhele1994@gmail.com>
2019-03-24 12:59:59 +0800

    fix(db): use mysql

--- src/init.go
+++ src/init.go
@@ -3,13 +3,13 @@ package src
 import (
 	"github.com/jmoiron/sqlx"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sqlx.DB
 
 func InitDB() {
-	dbConnect, err := sqlx.Open("sqlite3", DB_SRC)
+	dbConnect, err := sqlx.Open("mysql", DB_SRC)
 	if err != nil {
 		panic(err)
 	}
